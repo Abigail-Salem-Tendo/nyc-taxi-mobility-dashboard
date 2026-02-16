ae6a1b2c930fc06548f0b9aa87bdb76b633e99f9
Abigail Salem Tendo <salemabigail10@gmail.com>
2026-02-16 20:22:23 +0200

    database schema design

--- database/db_design.sql
+++ database/db_design.sql
@@ -1,27 +1,28 @@
 -- Set Up the database
-CREATE DATABASE IF NOT EXISTS mobility_db;
-USE mobility_db;
+CREATE DATABASE IF NOT EXISTS nyc_mobility_data;
+USE nyc_mobility_data;
 
 -- Table to store values from taxi zones csv file
-CREATE TABLE dim_zones (
-    location_id INT PRIMARY KEY,
-    borough VARCHAR(50),
-    zone_name VARCHAR(100),
+CREATE TABLE zone_lookup
+(
+    location_id  INT PRIMARY KEY,
+    borough      VARCHAR(50),
+    zone_name    VARCHAR(100),
     service_zone VARCHAR(50),
 
     INDEX idx_borough (borough)
-) ENGINE=InnoDB;
+);
 
 -- Table to reference the taxi_zones shape file
-CREATE TABLE zone_geometry (
+CREATE TABLE zone_geo (
     location_id INT PRIMARY KEY,
     zone_geojson LONGTEXT NOT NULL,
-    CONSTRAINT fk_zone_geo FOREIGN KEY (location_id)
-        REFERENCES dim_zones(location_id)
-) ENGINE=InnoDB;
+    FOREIGN KEY (location_id)
+        REFERENCES zone_lookup(location_id)
+);
 
 -- Trips table for yellow_tripdata.csv file
-CREATE TABLE fact_trips (
+CREATE TABLE trip_data (
     trip_id INT AUTO_INCREMENT PRIMARY KEY,
     vendor_id TINYINT,
     pickup_datetime DATETIME NOT NULL,
@@ -43,31 +44,32 @@ CREATE TABLE fact_trips (
     day_of_week TINYINT,
     is_peak_hour BOOLEAN,
 
-    FOREIGN KEY (pulocation_id) REFERENCES dim_zones(location_id),
-    FOREIGN KEY (dolocation_id) REFERENCES dim_zones(location_id),
+    FOREIGN KEY (pulocation_id) REFERENCES zone_lookup(location_id),
+    FOREIGN KEY (dolocation_id) REFERENCES zone_lookup(location_id),
 
     CHECK (trip_distance >= 0),
     CHECK (fare_amount >= 0),
     CHECK (pickup_datetime < dropoff_datetime)
-) ENGINE=InnoDB;
+);
 
 -- Add Performance Indexes to help with dashboard lookup
-CREATE INDEX idx_pickup_time ON fact_trips(pickup_datetime);
-CREATE INDEX idx_hour_of_day ON fact_trips(hour_of_day);
-CREATE INDEX idx_peak_hour ON fact_trips(is_peak_hour);
-CREATE INDEX idx_pulocation ON fact_trips(pulocation_id);
-CREATE INDEX idx_dolocation ON fact_trips(dolocation_id);
-CREATE INDEX idx_od_pair ON fact_trips(pulocation_id, dolocation_id);
-CREATE INDEX idx_congestion_level ON fact_trips(congestion_level);
+CREATE INDEX idx_pickup_time ON trip_data(pickup_datetime);
+CREATE INDEX idx_hour_of_day ON trip_data(hour_of_day);
+CREATE INDEX idx_peak_hour ON trip_data(is_peak_hour);
+CREATE INDEX idx_pulocation ON trip_data(pulocation_id);
+CREATE INDEX idx_dolocation ON trip_data(dolocation_id);
+CREATE INDEX idx_od_pair ON trip_data(pulocation_id, dolocation_id);
+CREATE INDEX idx_congestion_level ON trip_data(congestion_level);
 
 -- a table to show the excluded data logs
-CREATE TABLE excluded_data_log (
-    log_id INT AUTO_INCREMENT PRIMARY KEY,
-    issue_type VARCHAR(50) NOT NULL,
-    trip_identifier VARCHAR(255),
-    field_name VARCHAR(50),
+CREATE TABLE excluded_data_log
+(
+    log_id            INT AUTO_INCREMENT PRIMARY KEY,
+    issue_type        VARCHAR(50) NOT NULL,
+    trip_identifier   VARCHAR(255),
+    field_name        VARCHAR(50),
     issue_description TEXT,
-    action_taken VARCHAR(100),
+    action_taken      VARCHAR(100),
 
     INDEX idx_issue_type (issue_type)
-) ENGINE=InnoDB;
+);
